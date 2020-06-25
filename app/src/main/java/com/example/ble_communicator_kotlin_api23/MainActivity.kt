5f1e320db5cfb08df066ac7411258191f3cf3762
Pablo <pgonzale@ualberta.ca>
2020-06-25 12:05:38 -0600

    Following Android Developers tutorial

--- app/src/main/java/com/example/ble_communicator_kotlin_api23/MainActivity.kt
+++ app/src/main/java/com/example/ble_communicator_kotlin_api23/MainActivity.kt
@@ -4,8 +4,6 @@ import android.app.ListActivity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -13,63 +11,55 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import android.widget.ArrayAdapter
 import android.widget.Toast
 
 private const val SCAN_PERIOD: Long = 1000
 
 class MainActivity : AppCompatActivity() {
-    private val bleScanner = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult?) {
-            super.onScanResult(callbackType, result)
-            Log.d(
-                "DeviceListActivity",
-                "onScanResult: ${result?.device?.address} - ${result?.device?.name}"
-            )
-        }
-
-        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
-            super.onBatchScanResults(results)
-            Log.d("DeviceListActivity", "onBatchScanResults:${results.toString()}")
-        }
-
-        override fun onScanFailed(errorCode: Int) {
-            super.onScanFailed(errorCode)
-            Log.d("DeviceListActivity", "onScanFailed: $errorCode")
-        }
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("DeviceListActivity", "onCreate()")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device_list)
-
-
+        setContentView(R.layout.activity_main)
     }
 
-    private val bluetoothLeScanner: BluetoothLeScanner
-        get() {
-            val bluetoothManager =
-                applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-            val bluetoothAdapter = bluetoothManager.adapter
-            return bluetoothAdapter.bluetoothLeScanner
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val BluetoothAdapter.isDisabled: Boolean
+        get() = !isEnabled
+
+    fun checkBluetooth() {
+        val REQUEST_ENABLE_BT = 1
+        bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
+            val enableBTIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT)
         }
-
-    class ListDevicesAdapter(context: Context?, resource: Int) :
-        ArrayAdapter<String>(context!!, resource)
-
-    override fun onStart() {
-        Log.d("DeviceListActivity", "onStart()")
-        super.onStart()
-
-        bluetoothLeScanner.startScan(bleScanner)
-
     }
 
-    override fun onStop() {
-        bluetoothLeScanner.stopScan(bleScanner)
-        super.onStop()
+}
+
+class DeviceScanActivity(
+    private val bluetoothAdapter: BluetoothAdapter,
+    private val handler: Handler,
+    private val
+) : ListActivity() {
+    private var mScanning: Boolean = false
+
+    private fun scanLeDevice(enable: Boolean) {
+        when (enable) {
+            true -> {
+                handler.postDelayed({
+                    mScanning = false
+
+                }, SCAN_PERIOD)
+                mScanning = true
+
+
+            }
+            else -> {
+                mScanning = false
+            }
+        }
     }
 }
