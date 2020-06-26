dcec21f82354da1c6653e1eb720c10ffb3a88b08
Pablo <pgonzale@ualberta.ca>
2020-06-26 12:12:08 -0600

    Scan layout completed. Currently focused on BLE scan function

--- app/src/main/java/com/example/ble_communicator_kotlin_api23/MainActivity.kt
+++ app/src/main/java/com/example/ble_communicator_kotlin_api23/MainActivity.kt
@@ -11,13 +11,22 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
+
 private const val SCAN_PERIOD: Long = 1000
+const val EXTRA_MESSAGE = "com.example.ble_communicator_kotlin_api23.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when user taps Scan button */
+    fun scanBLE(view: View) {
+        // Scan BLE devices in area
     }
 }
