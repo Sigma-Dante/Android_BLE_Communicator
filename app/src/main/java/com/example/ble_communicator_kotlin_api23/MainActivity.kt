cf1315fb3b829a6a897137848ddd386aef0b753c
Pablo G <pgonzale@ualberta.ca>
2020-07-02 21:58:09 -0600

    latest

--- app/src/main/java/com/example/ble_communicator_kotlin_api23/MainActivity.kt
+++ app/src/main/java/com/example/ble_communicator_kotlin_api23/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.ble_communicator_kotlin_api23
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.le.*
-import android.bluetooth.le.ScanSettings.CALLBACK_TYPE_ALL_MATCHES
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -12,10 +13,6 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.PermissionChecker
-import com.polidea.rxandroidble2.RxBleClient
-import io.reactivex.disposables.Disposable
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,81 +21,92 @@ class MainActivity : AppCompatActivity() {
     private val REQUEST_ENABLE_BLUETOOTH = 1
     private val REQUEST_ENABLE_LOCATION = 1
     private val mBluetoothLeScanner: BluetoothLeScanner? = null
-    private val deviceaddress = "CE:01:23:D9:13:BE"
+    private val deviceMACAddress = "CE:01:23:D9:13:BE"
+    private val TAG = MainActivity::class.java.simpleName
 
     // RxAndroidBLE Library Specific
     // private val rxBleClient = RxBleClient.create(this)
     // private val scanDisposable: Disposable? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
-    override fun onStart() {
-        Log.d("ScanDeviceActivity", "onStart()")
-        super.onStart()
-
-        // Check and ask for fine location needed for BLE scanning
-        when (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            PackageManager.PERMISSION_GRANTED -> mBluetoothLeScanner?.startScan(blescancallback)
-            else -> requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ENABLE_LOCATION)
-        }
-
-        // Check if phone has Bluetooth
-        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
-            Toast.makeText(
-                this,
-                "Bluetooth is not supported on this device",
-                Toast.LENGTH_SHORT
-            ).show();
-        }
-
+    private fun hasBluetoothLE(){
         // Check if phone has Bluetooth LE
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(
                 this,
                 "Bluetooth Low Energy is not supported on this device",
                 Toast.LENGTH_SHORT
-            ).show();
+            ).show()
+            Log.d(TAG, "Bluetooth LE not supported on device")
         }
 
         // Checks if bluetooth adapter is disabled or not and if disabled ask user to enable bluetooth
         mbluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (mbluetoothAdapter == null) {
             Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_LONG).show()
+            Log.d(TAG, "No Bluetooth module in device")
         }
         if (!mbluetoothAdapter!!.isEnabled) {
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+            Log.d(TAG, "Requesting to enable Bluetooth...")
+        }
+        else {
+            Log.d(TAG, "Bluetooth already enabled")
         }
     }
+
+    private fun checkLocation() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permission already granted: ACCESS_FINE_LOCATION")
+        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_ENABLE_LOCATION)
+            Log.d(TAG, "Had to request access to fine location....")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate()")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart()")
+        super.onStart()
+        hasBluetoothLE()
+        checkLocation()
+        }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
             1 -> when (grantResults) {
                 intArrayOf(PackageManager.PERMISSION_GRANTED) -> {
-                    Log.d("ScanDevices", "onRequestPermissionsResult(PERMISSION_GRANTED)")
-                    mBluetoothLeScanner?.startScan(blescancallback)
+                    Log.d(TAG, "Permission request Granted: Access Fine Location")
                 }
                 else -> {
-                    Log.d("ScanDevices", "onRequestPermissionsResult(not PERMISSION_GRANTED)")
+                    Log.d(TAG, "Permission request Denied: Access Fine Location")
                 }
             }
-            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
 
     private fun startBLEScan() {
-        var resultStart = mBluetoothLeScanner?.startScan(blescancallback)
+        mBluetoothLeScanner?.startScan(blescancallback)
         Toast.makeText(this, "Scanning...", Toast.LENGTH_LONG).show()
-        Log.i("Log", String.format("BLE Scan Start\" = %d", resultStart))
+        Log.d(TAG, "************************************************************************")
+        Log.d(TAG, "Beginning BLE Scan...")
     }
 
     private fun stopBLEScan() {
-        var resultStop = mBluetoothLeScanner?.stopScan(blescancallback)
-        Toast.makeText(this, "", Toast.LENGTH_LONG).show()
-        Log.i("Log", String.format("BLE Scan End\" = %d", resultStop))
+        mBluetoothLeScanner?.stopScan(blescancallback)
+        mBluetoothLeScanner?.flushPendingScanResults(blescancallback)
+        Toast.makeText(this, "Scan end", Toast.LENGTH_LONG).show()
+        Log.d(TAG, "Ending BLE Scan...")
+        Log.d(TAG, "------------------------------------------------------------------------")
     }
 
     /** Called when user taps Scan button */
@@ -106,7 +114,7 @@ class MainActivity : AppCompatActivity() {
         startBLEScan()
         Handler().postDelayed({
             stopBLEScan()
-        }, 5000)
+        }, 20000)
     }
 }
 
@@ -114,22 +122,20 @@ private val blescancallback = object :ScanCallback() {
 
     override fun onScanResult(callbackType: Int, result: ScanResult?) {
         super.onScanResult(callbackType, result)
-        val device = result!!.device
-        Log.i("Log", "******************************************")
-        Log.i("Log", "The scan result: $result");
-        Log.i("Log", "------------------------------------------");
-        //Log.i("Log","onScanResult: ${result.device?.address} - ${result.device?.name}")
+        println(result?.device?.name)
+        Log.d(
+            "DeviceListActivity",
+            "onScanResult: ${result?.device?.address} - ${result?.device?.name}"
+        )
     }
 
     override fun onBatchScanResults(results: MutableList<ScanResult>?) {
         super.onBatchScanResults(results)
-        Log.i("Log", "onBatchScanResults")
-        Log.i("Log","onBatchScanResults:${results.toString()}")
+        Log.d("DeviceListActivity","onBatchScanResults:${results.toString()}")
     }
 
     override fun onScanFailed(errorCode: Int) {
         super.onScanFailed(errorCode)
-        Log.i("Log", "onScanFailed")
-        Log.i("Log", "onScanFailed: $errorCode")
+        Log.d("DeviceListActivity", "onScanFailed: $errorCode")
     }
 }
